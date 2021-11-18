197b1670e2b7960e441d46334ea6e22d3090a0e2
newMoon4578 <87790930+newMoon4578@users.noreply.github.com>
2021-11-17 19:22:28 -0800

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("I always run, but I never walk. What am I?", 100,50)
+    fill("lightblue")
+    text("Hint: It also has a bank!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("You buy me to eat, but you never eat me. What am I?", 700,150)
+    fill("lightblue")
+    text("Hint: What do we eat on?", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("If you say my name, you break me. What am I?", 100,250)
+    fill("lightblue")
+    text("Hint: What is the opposite of loud?", 100,270)
+
+}
